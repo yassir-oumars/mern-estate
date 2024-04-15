898909c15c52341e019e688c63aea8fb1377cf76
Yassir Oumars <oumyassire555@gmail.com>
2024-04-15 18:02:17 +0100

    add sign out functionnality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -45,10 +45,21 @@ const userSlice = createSlice({
         deleteUserFailure: (state, action) => {
             state.error = action.payload
             state.loading = false
+        },signOutUserStart: (state) => {
+            state.loading =  true
+        },
+        signOutUserSuccess: (state) => {
+            state.currentUser = null
+            state.loading = false
+            state.error = null
+        },
+        signOutUserFailure: (state, action) => {
+            state.error = action.payload
+            state.loading = false
         },   
     }
 })
 
-export const {signInStart, signInFailure, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess} = userSlice.actions
+export const {signInStart, signInFailure, signInSuccess, updateUserFailure, updateUserStart, updateUserSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart,signOutUserSuccess, signOutUserFailure } = userSlice.actions
 
 export const userReducer = userSlice.reducer;
