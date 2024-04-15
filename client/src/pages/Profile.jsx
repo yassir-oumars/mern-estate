4c7a08a6aefccc80a8620c64fbf325d4d64c960d
Yassir Oumars <oumyassire555@gmail.com>
2024-04-15 16:01:33 +0100

    delete user functionnality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useRef, useState, useEffect} from 'react'
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import {app} from '../firebase.js'
-import {updateUserStart, updateUserSuccess, updateUserFailure} from '../redux/user/userSlice.js'
+import {updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess} from '../redux/user/userSlice.js'
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -70,6 +70,21 @@ const handleSubmit = async (e) => {
   }
 }
 
+const handleDeleteUser = async () => {
+  try {
+    dispatch(deleteUserStart())
+    const res = await fetch(`/api/user/delete/${currentUser._id}`,{method: 'DELETE',})
+    const data = await res.json()
+    if (data.success === false) {
+      dispatch(deleteUserFailure(data.message))
+      return
+    }
+    dispatch(deleteUserSuccess(data))
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message))
+  }
+}
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7' >Profile</h1>
@@ -90,7 +105,7 @@ const handleSubmit = async (e) => {
        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button> 
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
